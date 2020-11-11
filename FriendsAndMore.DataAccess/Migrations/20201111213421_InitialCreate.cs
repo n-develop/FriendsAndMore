8e194f3b33ee5a3b0bcb0f356811eb8975bcdd8b
Lars Richter <github@n-develop.com>
2020-11-11 22:46:42 +0100

    Added first migration

---
+++ FriendsAndMore.DataAccess/Migrations/20201111213421_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FriendsAndMore.DataAccess.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Firstname = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Lastname = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ContactId", "Firstname", "Lastname" },
+                values: new object[] { 1, "Max", "Mustermann" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ContactId", "Firstname", "Lastname" },
+                values: new object[] { 2, "Manuela", "Mustermann" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ContactId", "Firstname", "Lastname" },
+                values: new object[] { 3, "John", "Smith" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
