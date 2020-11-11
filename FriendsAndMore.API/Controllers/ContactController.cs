15767fa36b1f465c126405a180db6eef15c399b6
Lars Richter <github@n-develop.com>
2020-11-11 22:46:31 +0100

    Added new controller

---
+++ FriendsAndMore.API/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FriendsAndMore.DataAccess.Entities;
+using FriendsAndMore.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FriendsAndMore.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> _logger;
+        private readonly IContactRepository _contactRepository;
+
+        public ContactController(ILogger<ContactController> logger, IContactRepository contactRepository)
+        {
+            _logger = logger;
+            _contactRepository = contactRepository;
+        }
+        
+        [HttpGet]
+        public async Task<IEnumerable<Contact>> Get()
+        {
+            return await _contactRepository.GetContacts();
+        }
+    }
+}
