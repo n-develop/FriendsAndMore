1561b8ab4de9a4ffda7bca2202cf380d165af07f
Lars Richter <github@n-develop.com>
2020-11-14 17:35:53 +0100

    Add HttpClient and service to DI container

--- FriendsAndMore.UI/Startup.cs
+++ FriendsAndMore.UI/Startup.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using FriendsAndMore.UI.Data;
+using FriendsAndMore.UI.Services;
 
 namespace FriendsAndMore.UI
 {
@@ -28,6 +25,15 @@ namespace FriendsAndMore.UI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddScoped(s =>
+            {
+                var client = new HttpClient { BaseAddress = new Uri("https://localhost:5001/") }; 
+                return client;
+            });
+
+            services.AddScoped<IContactService, ContactService>();
+            
+            // TODO remove this sample stuff
             services.AddSingleton<WeatherForecastService>();
         }
 
