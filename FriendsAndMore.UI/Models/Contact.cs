1610686403fdff69355a81d92a1456907d3eddcd
Lars Richter <github@n-develop.com>
2020-11-14 17:34:58 +0100

    Added model for UI project

---
+++ FriendsAndMore.UI/Models/Contact.cs
@@ -0,0 +1,11 @@
+namespace FriendsAndMore.UI.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+    }
+}
