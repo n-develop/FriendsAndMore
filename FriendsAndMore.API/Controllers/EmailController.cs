7e1c948c89c4a599cc75f4c159dcb2b13b5c114d
Lars Richter <github@n-develop.com>
2020-11-20 22:43:29 +0100

    Added update method in controller

--- FriendsAndMore.API/Controllers/EmailController.cs
+++ FriendsAndMore.API/Controllers/EmailController.cs
@@ -21,5 +21,29 @@ namespace FriendsAndMore.API.Controllers
         {
             return await _emailAddressRepository.GetEmailAddressById(id);
         }
+        
+        [HttpPut]
+        public IActionResult Update([FromBody] EmailAddress emailAddress)
+        {
+            if (emailAddress == null)
+                return BadRequest();
+
+            if (emailAddress.Type == string.Empty || emailAddress.Email == string.Empty)
+            {
+                ModelState.AddModelError("Type/Email", "The type and email shouldn't be empty");
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var emailToUpdate = _emailAddressRepository.GetEmailAddressById(emailAddress.EmailAddressId);
+
+            if (emailToUpdate == null)
+                return NotFound();
+
+            _emailAddressRepository.UpdateEmailAddress(emailAddress);
+
+            return NoContent();
+        }
     }
 }
